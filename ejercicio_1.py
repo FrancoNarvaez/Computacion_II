72b53949acfd25a4c41ae4ee3d71d9edac63794c
FrancoNarvaez <102602463+FrancoNarvaez@users.noreply.github.com>
2023-03-28 13:09:11 -0300

    TP1
    4 ejercicios argparse

---
+++ ejercicio_1.py
@@ -0,0 +1,19 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num", type=int)
+    args = parser.parse_args()
+
+    if args.num > 0:
+        oddlist = []
+        for i in range(args.num):
+            if (args.num % 2) == 1:
+                oddlist.append(i)
+    else:
+        print("The number has to be positive")
+    print("odds numbers: ", oddlist)
+
+
+if __name__ == "__main__":
+    main()
