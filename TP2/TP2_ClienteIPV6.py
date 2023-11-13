1c3a27e65341ed4f523b2647cc96543a36eaec1f
Franco Narvaez <f.narvaez@alumno.um.edu.ar>
2023-11-13 19:30:48 -0300

    TP2 terminado con manejo de argparse

--- TP2/TP2_ClienteIPV6.py
+++ TP2/TP2_ClienteIPV6.py
@@ -1,21 +1,30 @@
+# Cliente
 import aiohttp
 import asyncio
+import argparse
+from datetime import datetime
 
 async def send_image_to_server(image_path):
-    with open(image_path, 'rb') as file:
-        image_data = file.read()
+    try:
+        with open(image_path, 'rb') as file:
+            image_data = file.read()
+        async with aiohttp.ClientSession() as session:
+            async with session.post('http://[::]:8888/process_image', data=image_data) as response:
+                response_data = await response.read()
+                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                with open(f'received_image_{timestamp}.jpg', 'wb') as out_file:
+                    out_file.write(response_data)
+                print('Received and saved image')
+    except Exception as e:
+        print(f"Error sending image to server: {e}")
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post('http://[::]:8888/process_image', data=image_data) as response:
-            response_data = await response.read()
+async def main(image_path):
+    await send_image_to_server(image_path)
 
-            # Guardar la imagen recibida en un archivo
-            with open('received_image.jpg', 'wb') as out_file:
-                out_file.write(response_data)
-
-            print('Received and saved image')
-
-async def main():
-    await send_image_to_server('/home/franconarvaez/Escritorio/Computacion_II/TP2/linuxImagen.jpeg')
-
-asyncio.run(main())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Send an image to the server.', add_help=False)
+    parser.add_argument('-i', '--image_path', type=str, required=True, help='The path to the image to send.')
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, 
+                        help='Show this help message and exit')
+    args = parser.parse_args()
+    asyncio.run(main(args.image_path))
--- TP2/TP2_Servidor.py
+++ TP2/TP2_Servidor.py
@@ -5,30 +5,30 @@ from PIL import Image
 from io import BytesIO
 
 async def process_image(content):
-    image = Image.open(BytesIO(content))
-    grayscale_image = image.convert('L')
-
-    output_buffer = BytesIO()
-    grayscale_image.save(output_buffer, format='JPEG')
-    processed_image = output_buffer.getvalue()
-    print('Processed image')
-    return processed_image
+    try:
+        image = Image.open(BytesIO(content))
+        grayscale_image = image.convert('L')
+        output_buffer = BytesIO()
+        grayscale_image.save(output_buffer, format='JPEG')
+        processed_image = output_buffer.getvalue()
+        print('Processed image')
+        return processed_image
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        return None
 
 async def handle_request(request):
     if request.method == 'POST':
         content = await request.read()
-
-        # Process the image
         processed_image = await process_image(content)
-
-        # Send the processed image to the client
-        return web.Response(body=processed_image, content_type='image/jpeg')
-        print('Sent processed image')
+        if processed_image is not None:
+            print('Sent processed image')
+            return web.Response(body=processed_image, content_type='image/jpeg')
+        else:
+            return web.Response(status=500)
     else:
         return web.Response(status=404)
 
 app = web.Application()
 app.router.add_route('*', '/process_image', handle_request)
-
-# Iniciar el servidor en todas las interfaces de red (IPv4 e IPv6)
 web.run_app(app, host='::', port=8888)
