9e251174c860955935ac908261cb4f3b87629384
Franco Narvaez <f.narvaez@alumno.um.edu.ar>
2024-03-26 13:37:45 -0300

    Cliente simplificcado y funciones en utils.py

--- final/client2.py
+++ final/client2.py
@@ -1,85 +1,14 @@
 from config import *
-import http.client
-import json
-import time
+from utils import send_get_request, send_post_request, get_task_status
 
-
-def send_get_request():
-    server_address = CLIENT2_IP
-    server_port = CLIENT2_PORT
-
-    conn = http.client.HTTPConnection(server_address, server_port)
-
-    conn.request("GET", "/menu")
-
-    response = conn.getresponse()
-
-    data = response.read()
-
-    print("Response from server:")
-    print(data.decode('utf-8'))
-
-    conn.close()
-
-
-def send_post_request():
-    server_address = CLIENT2_IP
-    server_port = CLIENT2_PORT
-
-    conn = http.client.HTTPConnection(server_address, server_port)
-
-    pedido = {
-        'productos': [
-            {'producto': 'Pizza', 'cantidad': 2}
-        ]
-    }
-
-    pedido_json = json.dumps(pedido)
-    headers = {'Content-type': 'application/json'}
-
-    conn.request("POST", "/pedido", pedido_json, headers)
-
-    print("\nPedido enviado")
-    response = conn.getresponse()
-
-    data = response.read()
-
-    print("Response from server:")
-    task_id = data.decode('utf-8')
-    print(task_id)
-
-    conn.close()
-
-    return task_id
-
-
-def get_task_status(task_id):
-    server_address = CLIENT2_IP
-    server_port = CLIENT2_PORT
-
-    while True:
-        try:
-            conn = http.client.HTTPConnection(server_address, server_port)
-
-            conn.request("GET", f"/status/{task_id}")
-
-            response = conn.getresponse()
-
-            data = response.read()
-
-            print("\nResponse from server:")
-            print(data.decode('utf-8'))
-            if data.decode('utf-8') == "Completado":
-                conn.close()
-                break
-            conn.close()
-            time.sleep(3)  # Espera 3 segundos antes de consultar de nuevo
-        except http.client.RemoteDisconnected:
-            print("Server disconnected. Retrying in 5 seconds...")
-            time.sleep(3)
+pedido = {
+    'productos': [
+        {'producto': 'Pizza', 'cantidad': 2}
+    ]
+}
 
 
 if __name__ == "__main__":
-    send_get_request()
-    task_id = send_post_request()
-    get_task_status(task_id)
+    send_get_request(CLIENT2_IP, CLIENT2_PORT)
+    task_id = send_post_request(CLIENT2_IP, CLIENT2_PORT, pedido)
+    get_task_status(CLIENT2_IP, CLIENT2_PORT, task_id)
--- final/utils.py
+++ final/utils.py
@@ -1,3 +1,77 @@
+import http.client
+import json
+import time
+
+
 def parse_arguments():
     #Manejar aca el parceo de manera generica para no repetir codigo
     pass
+
+
+def send_get_request(cliente_ip, cliente_port):
+    server_address = cliente_ip
+    server_port = cliente_port
+
+    conn = http.client.HTTPConnection(server_address, server_port)
+
+    conn.request("GET", "/menu")
+
+    response = conn.getresponse()
+
+    data = response.read()
+
+    print("Response from server:")
+    print(data.decode('utf-8'))
+
+    conn.close()
+
+
+def send_post_request(cliente_ip, cliente_port, pedido):
+    server_address = cliente_ip
+    server_port = cliente_port
+
+    conn = http.client.HTTPConnection(server_address, server_port)
+
+    pedido_json = json.dumps(pedido)
+    headers = {'Content-type': 'application/json'}
+
+    conn.request("POST", "/pedido", pedido_json, headers)
+
+    print("\nPedido enviado")
+    response = conn.getresponse()
+
+    data = response.read()
+
+    print("Response from server:")
+    task_id = data.decode('utf-8')
+    print(task_id)
+
+    conn.close()
+
+    return task_id
+
+
+def get_task_status(cliente_ip, cliente_port, task_id):
+    server_address = cliente_ip
+    server_port = cliente_port
+
+    while True:
+        try:
+            conn = http.client.HTTPConnection(server_address, server_port)
+
+            conn.request("GET", f"/status/{task_id}")
+
+            response = conn.getresponse()
+
+            data = response.read()
+
+            print("\nResponse from server:")
+            print(data.decode('utf-8'))
+            if data.decode('utf-8') == "Completado":
+                conn.close()
+                break
+            conn.close()
+            time.sleep(3)  # Espera 3 segundos antes de consultar de nuevo
+        except http.client.RemoteDisconnected:
+            print("Server disconnected. Retrying in 5 seconds...")
+            time.sleep(3)
